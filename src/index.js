1ba2086163bc8e704a934d1ab031afe702dd2728
shane_william_miller <shanemiller@iinet.net.au>
2025-01-22 18:03:29 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 // import express
-const express = require("express")
+const express = require("express");
 
-const postRouter = require("./routes/postRoutes")
+const postRouter = require("./routes/postRoutes");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
 app.get("/", (req, res) => {
-    res.json({
-        data: "Hello World!!"
-    })
-})
+  res.json({
+    data: "Hello World!!",
+  });
+});
 
 app.get("/hello", (req, res) => {
-    res.json({
-        data: "Another route named hello"
-    })
-})
+  res.json({
+    data: "Another route named hello",
+  });
+});
 
-app.use("/posts", postRouter)
+app.use("/posts", postRouter);
 
-app.listen(3000, () => {
-    console.log("Server started")
-})
+app.listen(3000, async () => {
+  console.log("Server started");
+  await mongoose.connect("mongodb://127.0.0.1:27017/blog_db");
+  console.log("Connected to MongoDB");
+});
