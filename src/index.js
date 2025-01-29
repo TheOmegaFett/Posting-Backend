9df52f4b513243efde9a1babd46ab028025550c1
shane_william_miller <shanemiller@iinet.net.au>
2025-01-29 18:09:01 +0800

    bug fix

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const mongoose = require("mongoose");
 const postRouter = require("./routes/postRoutes");
 const categoryRouter = require("./routes/categoryRoutes");
 const commentRouter = require("./routes/commentRoutes");
+const logger = require("./middlewares/logger");
+const checkIfAdmin = require("./middlewares/checkIfAdmin");
 
 const app = express();
 app.use(express.json());
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.json({
